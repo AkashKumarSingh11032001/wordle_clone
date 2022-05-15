851956633b1644e574d40ef2a6afbf2c9605def5
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-15 13:13:33 +0530

    additional supoort dart files

--- lib/widgets/wordle_keyboard.dart
+++ lib/widgets/wordle_keyboard.dart
@@ -1,12 +1,33 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:wordle_clone/widgets/wordle_key.dart';
 
 class WordleKeyboard extends StatelessWidget {
   const WordleKeyboard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Keyboard'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var i in "qwertyuiop".split("")) WordleKey(i)
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var i in "asdfghjkl".split("")) WordleKey(i)
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var i in "_zxcvbnm<".split("")) WordleKey(i)
+          ],
+        ),
+      ],
     );
   }
 }
