0718d581af10fb47eab07cc9a462ced297e8fc8a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-15 12:47:46 +0530

    making widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-  import 'package:flutter/material.dart';
-import 'package:wordle_clone/widgets/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:wordle_clone/widgets/wordle_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WordlePage(),
     );
   }
 }
